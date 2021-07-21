c8a7dbecf3192f2a01bc801d521d56278bc231f1
Jada Desormeaux <JadaE789@gmail.com>
2021-07-21 00:24:15 -0400

    index.js provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from 'react-redux'
 
 import App from './App'
 import store from './utils/store';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <React.StrictMode> 
+    <Provider store={store}>
+      <Router> 
+        <App />
+      </Router>
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 )
