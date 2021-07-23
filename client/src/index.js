7b686c1804d5111509cad54ef732f63be15c2d53
Jada Desormeaux <JadaE789@gmail.com>
2021-07-23 00:08:41 -0400

    index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { ApolloProvider } from '@apollo/react-hooks';
 
 ReactDOM.render(
-    <ApolloProvider client={client}> 
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </ApolloProvider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
